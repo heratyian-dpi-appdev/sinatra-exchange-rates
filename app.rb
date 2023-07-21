2c01646db8c5ee94fdf57f7b44d6c1664b131b28
Ian Heraty <heratyian@gmail.com>
2023-07-21 22:00:45 +0000

    Add solution

--- app.rb
+++ app.rb
@@ -1,9 +1,28 @@
 require "sinatra"
 require "sinatra/reloader"
 
+require "http"
+require "json"
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  response = HTTP.get("https://api.exchangerate.host/symbols")
+  @parsed_response = JSON.parse(response.body.to_s) if response.status.success?
+
+  erb(:root)
+end
+
+get("/:from_currency") do
+  @from_currency = params.fetch(:from_currency)
+  response = HTTP.get("https://api.exchangerate.host/symbols")
+  @parsed_response = JSON.parse(response.body.to_s) if response.status.success?
+  erb(:from_currency)
+end
+
+get("/:from_currency/:to_currency") do
+  @from_currency = params.fetch(:from_currency)
+  @to_currency = params.fetch(:to_currency)
+  response = HTTP.get("https://api.exchangerate.host/convert?from=#{@from
+  }&to=#{@to_currency}")
+  @parsed_response = JSON.parse(response.body.to_s) if response.status.success?
+  erb(:to_currency)
 end
